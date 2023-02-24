c534318ffc75eae1b13cc5ffd0285a67bcc726bf
Aleksei Teterin <alexey_teterin@mail.ru>
2023-02-24 22:50:00 +0300

    добавлены: Read by Id, Create, Delete, Update

--- OsDbWebApi/Program.cs
+++ OsDbWebApi/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OsDbWebApi.Models;
+using OsDbWebApi.Models.Entity;
+using System;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,14 +13,55 @@ var app = builder.Build();
 
 app.MapGet("/ping", async(context) => {
 	await context.Response.WriteAsync("pong");
-
-
-
 });
 
-app.MapGet("/all", async (HttpContext context, ApplicationDbContext db) =>
+// чтение всех записей из таблицы (R)
+app.MapGet("/get_notations", async (HttpContext context, ApplicationDbContext db) =>
 {
 	return await db.EntityOperationSystems.ToListAsync();
 });
+
+// чтение записи по id (R (by id))
+app.MapGet("/get_notation", async ([FromServices] ApplicationDbContext db,
+    [FromQuery] int id) => await db.EntityOperationSystems
+    .FirstOrDefaultAsync(p => p.Id == id));
+
+
+// (C) добавление новой записи в таблицу
+app.MapPost("/add_new_notation", async (HttpContext context, ApplicationDbContext db) =>
+{
+	//извлекаем данные из запроса
+	OperationSystems? entityOperationSystems = await context.Request.ReadFromJsonAsync<OperationSystems>();
+	// добавить запись если не null
+	if (entityOperationSystems != null)
+	{
+		db.EntityOperationSystems.Add(entityOperationSystems); // add to collection
+		db.SaveChanges(); // save changes to DB
+	}
+	return entityOperationSystems; // return added entity
+});
+
+// удаление записи по id (D)
+app.MapPost("/delete_notation",
+    async ([FromServices] ApplicationDbContext db,
+    [FromQuery] int id) =>
+    {
+        var currentOs = await db.EntityOperationSystems.FindAsync(id);
+        db.EntityOperationSystems.Remove(currentOs!);
+        await db.SaveChangesAsync();
+    });
+
+// (U) обновление записи по id
+app.MapPost("/update_notation",
+    async ([FromServices] ApplicationDbContext db,
+        [FromQuery] int id, string description, int version) =>
+    {
+        var currentOs = await db.EntityOperationSystems.FindAsync(id);
+        currentOs!.Description = description;
+        currentOs!.VersionNumber = version;
+
+        await db.SaveChangesAsync();
+    });
+
 
 app.Run();
